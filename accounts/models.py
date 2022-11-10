2c52a5e5806e5e1836c9081fcdef9f97f841d427
JOO-10000-Zi <j4615545@gmail.com>
2022-11-10 16:37:55 +0900

    시용자 모델 수정 및 팔로우 비동기 구현

--- accounts/models.py
+++ accounts/models.py
@@ -18,6 +18,10 @@ class User(AbstractUser):
         format="JPEG",
         options={"quality": 60},
     )
+    like_products = models.ManyToManyField("products.Product", related_name="like_zzim")
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="follow"
+    )
 
     def profile_image1(self):
         if self.profile_image and hasattr(self.profile_image, "url"):
