0fc8a5765727499424b578f422110851956ef40f
Gkhy <syan1022@naver.com>
2022-11-10 12:02:13 +0900

    review CRUD bone

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "reviews"
 
 urlpatterns = [
-    path("", views.index, name="index"),
-]
+    path("", views.main, name="main"),
+    path("<int:product_pk>/", views.index, name="index"),
+    path("<int:product_pk>/create/", views.create, name="create"),
+    path("delete/<int:product_pk>/", views.delete, name="delete"),
+    path("update/<int:review_pk>/", views.update, name="update"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from products.models import Product
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
-def index(request):
-    return render(request, "reviews/index.html")
+def main(request):
+    return render(request, "reviews/main.html")
+
+
+def index(request, product_pk):
+    product = Product.objects.get(pk=product_pk)
+    reviews = Product.review_set.all()
+    grade = 0
+    cnt = 0
+    for review in reviews:
+        grade += review.grade
+        cnt += 1
+    grade /= cnt
+    context = {
+        "reviews": reviews,
+        "product": product,
+        "grade": grade,
+    }
+    return render(request, "reviews/index.html", context)
+
+
+def create(request, product_pk):
+    product = Product.objects.get(pk=product_pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, request.FILES)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.product = product
+            review.account = request.user
+            review.save()
+            return redirect("reviews:index", product_pk)
+    else:
+        review_form = ReviewForm()
+    context = {
+        "review_form": review_form,
+    }
+    return render(request, "reviews/create.html", context)
+
+
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    review.delete()
+    return redirect("reviews:index", review.product.pk)
+
+
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST, request.FILES, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:index", review.product.pk)
+    else:
+        review_form = ReviewForm(instance=review)
+    context = {"review_form": review_form}
+    return render(request, "reviews/update.html", context)
