13e035bf69c715b335385eeb95896d7e24b86744
chaehyun_oh <ohchaehh@gmail.com>
2022-11-14 16:40:45 +0900

    메인 인덱스 css 수정, slide 추가

--- static/script/script.js
+++ static/script/script.js
@@ -12,6 +12,77 @@ const qnaBtnOpenModal = document.querySelector('.qna-modal-btn');
 const detailArea = document.querySelector('#product-detail');
 const body = document.querySelector('body');
 
+const slider = function () {
+    const slides = document.querySelectorAll('.slide');
+    const btnLeft = document.querySelector('.slider-btn-left');
+    const btnRight = document.querySelector('.slider-btn-right');
+    const dotContainer = document.querySelector('.dots');
+
+    let curSlide = 0;
+    const maxSlide = slides.length;
+
+    const createDots = function () {
+        slides.forEach(function (_, i) {
+            dotContainer.insertAdjacentHTML('beforeend', `<button class="dots-dot" data-slide="${i}"></button>`);
+        })
+    };
+    const activeDot = function (slide) {
+        document.querySelectorAll('.dots-dot').forEach(dot => dot.classList.remove('dots-dot-active'));
+        document.querySelector(`.dots-dot[data-slide="${slide}"]`).classList.add('dots-dot-active');
+    };
+
+    const goToSlide = function (slide) {
+        slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+    }
+
+    const nextSlide = function () {
+        if (curSlide === maxSlide - 1) {
+            curSlide = 0;
+        } else {
+            curSlide++;
+        }
+
+        goToSlide(curSlide);
+        activeDot(curSlide);
+    };
+
+    const prevSlide = function () {
+        if (curSlide === maxSlide - 1) {
+            curSlide = 0;
+        } else {
+            curSlide--;
+        }
+
+        goToSlide(curSlide);
+        activeDot(curSlide);
+    };
+    const init = function () {
+        createDots();
+        goToSlide(0);
+        activeDot(0);
+    };
+
+    init();
+
+    btnLeft.addEventListener('click', prevSlide);
+    btnRight.addEventListener('click', nextSlide);
+
+    document.addEventListener('keydown', function (e) {
+        if (e.key === "ArrowLeft") prevSlide();
+        e.key === "ArrowRight" && nextSlide();
+    });
+
+    dotContainer.addEventListener('click', function (e) {
+        if (e.target.classList.contains('dots-dot')) {
+            const { slide } = e.target.dataset;
+            goToSlide(slide);
+            activeDot(slide);
+        }
+    });
+
+};
+
+slider();
 
 revBtnOpenModal.addEventListener('click', function (e) {
     e.preventDefault();
