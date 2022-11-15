3b580e564a7d7104126268bc180acf1cfff304b3
hyejinny97 <yhj970102@gmail.com>
2022-11-15 17:46:52 +0900

    메인페이지 할인상품/MP 픽 데이터 연결

--- config/views.py
+++ config/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 def index(request):
-    return render(request, "index.html")
+    sale_under_40 = get_object_or_404(Product, pk=26)
+    sale_under_30 = get_object_or_404(Product, pk=47)
+    sale_under_20 = get_object_or_404(Product, pk=129)
+    weekly_planner = get_object_or_404(Product, pk=65)
+    monthly_planner = get_object_or_404(Product, pk=57)
+    photo_album = get_object_or_404(Product, pk=87)
+    
+    context = {
+        'sale_under_40': sale_under_40,
+        'sale_under_30': sale_under_30,
+        'sale_under_20': sale_under_20,
+        'weekly_planner': weekly_planner,
+        'monthly_planner': monthly_planner,
+        'photo_album': photo_album,
+    }
+
+    return render(request, "index.html", context)
