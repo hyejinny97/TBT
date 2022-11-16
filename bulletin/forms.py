a7f50ffbb64c4313148525ec7184e949e73e8213
Gkhy <syan1022@naver.com>
2022-11-16 16:51:50 +0900

    qa

--- bulletin/forms.py
+++ bulletin/forms.py
@@ -5,10 +5,9 @@ from .models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ["name", "content", "category"]
+        fields = ["content", "category"]
 
         labels = {
-            "name": "상품명",
             "content": "문의내용",
             "category": "문의유형",
         }
