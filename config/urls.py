c77844dd7dad55fc2b6bc89b1eae90c27ea179a8
hyejinny97 <yhj970102@gmail.com>
2022-11-17 18:49:46 +0900

    주문/결제 기능 구현

--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("reviews/", include("reviews.urls")),
     path("bulletin/", include("bulletin.urls")),
     path("cart/", include("cart.urls")),
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
+def index(request):
+    products = [Product.objects.get(pk=10), Product.objects.get(pk=20), Product.objects.get(pk=30)]
+ 
+    context = {
+        'products': products,
+    }
+    return render(request, 'orders/ordering.html', context)
